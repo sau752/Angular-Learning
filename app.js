2783a82b9615a17cb21ab780217e33b067589801
Saurabh Verma <v-saurv@microsoft.com>
2017-02-22 17:50:34 +0530

    Added Login Form and implemented Angular Routes

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-(function() {
-  var app = angular.module('gemStore', ['store-directives']);
+(function () {
+  var app = angular.module('gemStore', ['store-directives', 'ngRoute']);
 
-  app.controller('StoreController', ['$http', function($http){
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when("/", {
+        templateUrl: "login.html"
+      }).when("/product-home", {
+        templateUrl: "product-home.html"
+      }).otherwise({
+        redirectTo: '/'
+      });
+  });
+
+  app.controller('LoginFormController', ['$scope', '$location', function ($scope, $location) {
+    $scope.username = "";
+    $scope.password = "";
+    $scope.submit = function () {
+      if($scope.username === 'admin' && $scope.password === 'password') {
+        $location.path('product-home');
+      }
+    };
+
+  }]);
+
+  app.controller('StoreController', ['$http', function ($http) {
     var store = this;
     store.products = [];
-    $http.get('./store-products.json').success(function(data){
-        store.products = data;
+    $http.get('./store-products.json').success(function (data) {
+      store.products = data;
     });
   }]);
 
-  app.controller('ReviewController', function() {
+  app.controller('ReviewController', function () {
     this.review = {};
 
-    this.addReview = function(product) {
+    this.addReview = function (product) {
       product.reviews.push(this.review);
 
       this.review = {};
